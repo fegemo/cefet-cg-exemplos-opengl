af9c8adc30db5cb73df6a32baa73b1c9ea35378c
Flavio Coutinho <fegemo@gmail.com>
2016-03-04 10:14:36 -0300

    Adds quadrados-callback example

---
+++ quadrado-callbacks/main.c
@@ -0,0 +1,88 @@
+#include "GL/glew.h"  // use sinal maior,menor
+#include "GL/freeglut.h"
+
+// Rotina de desenho
+void desenhaMinhaCena()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(0.0, 1.0, 0.0);
+    // Desenha um polígono por seus vértices
+
+    glBegin(GL_TRIANGLE_FAN);
+        // NOVIDADE: antes os valores eram -0.5, 0.5
+        glVertex3f(20.0, 20.0, 0.0);
+        glVertex3f(80.0, 20.0, 0.0);
+        glVertex3f(80.0, 80.0, 0.0);
+        glVertex3f(20.0, 80.0, 0.0);
+    glEnd();
+
+    glFlush();
+}
+
+// NOVIDADE: uma função que vamos chamar dentro
+//    do "main"
+// Inicia algumas variáveis de estado do OpenGL
+void setup()
+{
+    // define qual é a cor do fundo
+    glClearColor(1.0, 1.0, 1.0, 0.0); // branco
+
+    // desenho preenchido vs. contorno
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+}
+
+// NOVIDADE: callback para o evento "reshape"
+void redimensionada(int width, int height)
+{
+   // left, bottom, right, top
+   glViewport(0, 0, width, height);
+
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
+
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+}
+
+// NOVIDADE: callback de "keyboard"
+void teclaPressionada(unsigned char key, int x, int y)
+{
+    // vê qual tecla foi pressionada
+    switch(key)
+    {
+    case 27:      // Tecla "ESC"
+        exit(0);  // Sai da aplicação
+        break;
+    default:
+        break;
+    }
+}
+
+// Função principal
+int main(int argc, char** argv)
+{
+   glutInit(&argc, argv);
+
+   glutInitContextVersion(1, 1);
+   glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+
+   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+   glutInitWindowSize(500, 500);
+   glutInitWindowPosition(100, 100);
+
+   glutCreateWindow("Quadrado");
+
+   // Registra callbacks para eventos
+   glutDisplayFunc(desenhaMinhaCena);
+   glutReshapeFunc(redimensionada);
+   glutKeyboardFunc(teclaPressionada);
+
+   // Configura valor inicial de algumas
+   // variáveis de estado do OpenGL
+   setup();
+
+   glutMainLoop();
+   return 0;
+}
