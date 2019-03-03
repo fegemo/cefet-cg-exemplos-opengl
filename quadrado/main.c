368549cc8ee5f0c43dafdc279acbf11aca452ef3
Flávio Coutinho <fegemo@gmail.com>
2019-03-03 19:55:43 -0300

    Simplifies quadrado to be close to the slides

--- quadrado/main.c
+++ quadrado/main.c
@@ -1,84 +1,51 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
-void desenhaCena(void)
-{
-    // Limpa a tela (com a cor definida por glClearColor(r,g,b)) para que
+void desenhaMinhaCena() {
+    // cor para limpar a tela
+    glClearColor(1, 1, 1, 1);      // branco
+
+    // limpa a tela (com a cor definida por glClearColor(r,g,b)) para que
     // possamos desenhar
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Começa a usar a cor verde
+    // começa a usar a cor verde
     glColor3f(0, 1, 0);
 
-    // Começa a desenhar um polígono com os vértices especificados
-    glBegin(GL_POLYGON);
-        glVertex3f(20, 20, 0);
-        glVertex3f(80, 20, 0);
-        glVertex3f(80, 80, 0);
-        glVertex3f(20, 80, 0);
+    // começa a desenhar um polígono com os vértices especificados
+    glBegin(GL_TRIANGLE_FAN);
+        glVertex3f(-0.5, -0.5, 0.0);
+        glVertex3f( 0.5, -0.5, 0.0);
+        glVertex3f( 0.5,  0.5, 0.0);
+        glVertex3f(-0.5,  0.5, 0.0);
     glEnd();
 
-    // Diz ao OpenGL para colocar o que desenhamos na tela
+    // diz ao OpenGL para colocar o que desenhamos na tela
     glFlush();
 }
 
-// Inicia algumas variáveis de estado
-void inicializa(void)
-{
-    // cor para limpar a tela
-    glClearColor(1, 1, 1, 0);      // branco
-}
 
-// Callback de redimensionamento
-void redimensiona(int w, int h)
-{
-   glViewport(0, 0, w, h);
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   glOrtho(0, 100, 0, 100, -1, 1);
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
-}
-
-// Callback de evento de teclado
-void teclado(unsigned char key, int x, int y)
-{
-   switch(key)
-   {
-      // Tecla ESC
-      case 27:
-         exit(0);
-         break;
-      default:
-         break;
-   }
-}
-
-// Rotina principal
 int main(int argc, char **argv)
 {
-    // Acordando o GLUT
+    // acordando o GLUT
     glutInit(&argc, argv);
 
-    // Definindo a versão do OpenGL que vamos usar
+    // definindo a versão do OpenGL que vamos usar
     glutInitContextVersion(1, 1);
     glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
 
-    // Configuração inicial da janela do GLUT
+    // configuração inicial da janela do GLUT
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-    glutInitWindowSize(400, 400);
+    glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 100);
 
-    // Abre a janela
+    // abre a janela
     glutCreateWindow("Quadrado");
 
-    // Registra callbacks para alguns eventos
-    glutDisplayFunc(desenhaCena);
-    glutReshapeFunc(redimensiona);
-    glutKeyboardFunc(teclado);
-    inicializa();
+    // registra callbacks para alguns eventos
+    glutDisplayFunc(desenhaMinhaCena);
 
-    // Entra em loop e nunca sai
+    // entra em loop e nunca sai
     glutMainLoop();
     return 0;
 }
