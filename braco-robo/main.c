0df7ccc33a03fe34c1a22517e47e7d46c0685d76
Flávio Coutinho <fegemo@gmail.com>
2019-03-08 12:01:07 -0300

    Improves braco-robo's legibility

--- braco-robo/main.c
+++ braco-robo/main.c
@@ -1,33 +1,33 @@
 #include <GL/freeglut.h>
 #include <stdio.h>
 
+const float LARGURA_BRACO = 2.0;
 int anguloOmbro = 0, anguloCutuvelo = 0;
 
-void inicializa()
-{
+void inicializa() {
     glClearColor (0.0, 0.0, 0.0, 0.0);
     glShadeModel (GL_FLAT);
 }
 
 
-void redimensiona(int w, int h)
-{
-    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+void redimensiona(int w, int h) {
+    glViewport (0, 0, w, h);
 
-    glMatrixMode (GL_PROJECTION);
-    glLoadIdentity ();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
     // a origem do mundo está no centro da janela (o gluPerspective
     // é sempre assim)
     gluPerspective(65.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+
     // chega o mundo todo mais pro fundo, pra poder desenhar no z = 0
-    glTranslatef (0.0, 0.0, -5.0);
+    glTranslatef (0.0, 0.0, -6.0);
 }
 
 
-// o braço e ante-braço são cada um uma caixa de tamanho 2 no eixo x, 0,4 no y
+// o braço e ante-braço são cada um uma caixa de tamanho 2 no eixo x, 0.4 no y
 // e 1 de profundidade:
 //  _________
 // /________/|
@@ -36,14 +36,13 @@ void redimensiona(int w, int h)
 // a glutSolid|WireCube desenha o cubo na origem (e.g., -WIDTH/2, +WIDTH/2...)
 void desenhaParteDoBraco() {
     glPushMatrix();
-        glScalef (2.0, 0.4, 1.0);
-        glutWireCube (1.0);
+        glScalef(LARGURA_BRACO, 0.4, 1.0);
+        glutWireCube(1.0);
     glPopMatrix();
 }
 
 
-void desenha()
-{
+void desenha() {
     // entramos na desenha() com o sistema de coordenadas com origem no centro
     // da janela
 
@@ -59,9 +58,9 @@ void desenha()
         //     (não é mais "a direita do mundo")
         // (4) desenha o braço onde está nosso "cursor" (no 0,0 do sistema de
         //     coordenadas corrente)
-        glTranslatef (-1.0, 0.0, 0.0);
-        glRotatef ((GLfloat) anguloOmbro, 0.0, 0.0, 1.0);
-        glTranslatef (1.0, 0.0, 0.0);
+        glTranslatef(-LARGURA_BRACO/2, 0, 0);
+        glRotatef(anguloOmbro, 0, 0, 1);
+        glTranslatef(1, 0, 0);
         desenhaParteDoBraco();
 
         // ---------
@@ -75,9 +74,9 @@ void desenha()
         // (2) rotacionar o sistema y graus (ref. ao cutuvelo)
         // (3) andar +1 unidade no eixo x, para chegar até o centro do antebraço
         // (4) desneha o antebraço
-        glTranslatef (1.0, 0.0, 0.0);
-        glRotatef ((GLfloat) anguloCutuvelo, 0.0, 0.0, 1.0);
-        glTranslatef (1.0, 0.0, 0.0);
+        glTranslatef(LARGURA_BRACO/2, 0.0, 0.0);
+        glRotatef(anguloCutuvelo, 0.0, 0.0, 1.0);
+        glTranslatef(LARGURA_BRACO/2, 0.0, 0.0);
         glPushMatrix();
             desenhaParteDoBraco();
         glPopMatrix();
@@ -86,8 +85,7 @@ void desenha()
     glutSwapBuffers();
 }
 
-void teclado(unsigned char key, int x, int y)
-{
+void teclado(unsigned char key, int x, int y) {
    switch (key) {
       // Tecla ESC
       case 27:
@@ -114,23 +112,24 @@ void teclado(unsigned char key, int x, int y)
    }
 }
 
-int main(int argc, char** argv)
-{
-   glutInit(&argc, argv);
-   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-   glutInitWindowSize (500, 500);
-   glutInitWindowPosition (100, 100);
-   glutCreateWindow ("Braço do Robô");
-   inicializa();
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitContextVersion(1, 1);
+    glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Braco do Robo");
+    inicializa();
 
-   glutDisplayFunc(desenha);
-   glutReshapeFunc(redimensiona);
-   glutKeyboardFunc(teclado);
+    glutDisplayFunc(desenha);
+    glutReshapeFunc(redimensiona);
+    glutKeyboardFunc(teclado);
 
-   printf("Aperte:\n\n");
-   printf("\t- 'o' e 'O': para aumentar/diminuir rotação do 'ombro'\n");
-   printf("\t- 'c' e 'C': para aumentar/diminuir rotação do 'cutuvelo'\n\n");
+    printf("Aperte:\n\n");
+    printf("\t- 'o' e 'O': para aumentar/diminuir rotação do 'ombro'\n");
+    printf("\t- 'c' e 'C': para aumentar/diminuir rotação do 'cutuvelo'\n\n");
 
-   glutMainLoop();
-   return 0;
+    glutMainLoop();
+    return 0;
 }
